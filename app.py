9d9c09241a612e474f00983a2d2399ae6f9af95a
saito898 <saloaran1909@gmail.com>
2026-09-10 13:44:26 -0500

    Fix indentation for opening triste.json file

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ with st.expander('Analizar texto'):
               st.lottie(animation, width=350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
-             with open('triste.json') as source:
+            with open('triste.json') as source:
               animation= json.load(source)
               st.lottie(animation, width=350)
         else:
