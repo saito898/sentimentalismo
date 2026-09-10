5ac46a834d9a3a78a24a718568e9e948f1f3f301
saito898 <saloaran1909@gmail.com>
2026-09-10 13:44:43 -0500

    Fix indentation for loading neutral animation

--- app.py
+++ app.py
@@ -48,6 +48,6 @@ with st.expander('Analizar texto'):
               st.lottie(animation, width=350)
         else:
             st.write( 'Es un sentimiento Neutral 😐')
-             with open('neutral.json') as source:
+            with open('neutral.json') as source:
               animation= json.load(source)
               st.lottie(animation, width=350)
