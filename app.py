e21646c7eb1d30a33efc21d534a0e3fcb99697fa
saito898 <saloaran1909@gmail.com>
2026-09-10 13:40:01 -0500

    Replace image display with Lottie animations

--- app.py
+++ app.py
@@ -38,9 +38,16 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
-            st.image('perrito.json')
+            with open('perrito.json') as source:
+              animation= json.load(source)
+              st.lottie(animation, width=350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
-            st.image('triste.json')
+             with open('triste.json') as source:
+              animation= json.load(source)
+              st.lottie(animation, width=350)
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+             with open('neutral.json') as source:
+              animation= json.load(source)
+              st.lottie(animation, width=350)
